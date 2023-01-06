f98fb37cbd1a7467aec3c3272d30849e38347855
meehir_lokhande <meehirlokhande@gmail.com>
2023-01-06 12:46:42 +0530

    que2 done

--- Elementary/que2.py
+++ Elementary/que2.py
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-n=int(input())
-x=[]
-for i in range(n):
-    a= input().split(",")
-    b=0
-    for j in a:
-        if j.islower()==True:
-            b+=1
-        if b==len(a):
-            x.append(tuple(a))
-=======
-n=int(input())
-x=[]
-for i in range(n):
-    a= inpt().split(",")
-    b=0
-    for j in a:
-        if j.islower()==True:
-            b+=1
-        if b==len(a):
-            x.append(tuple(a))
->>>>>>> b261723a7a3d494711e40d3daea2069ebb6acbad
-    print(x)
+# 2. Write a program that asks the user for their name and greets them with their name.
+
+name = input("Enter your name : ")
+print("Hello " + name + "!")
